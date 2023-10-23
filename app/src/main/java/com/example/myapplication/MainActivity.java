13ca05058894aa4bfed1cecf72878477d0491708
Platonkin <Platonkin@gmail.com>
2023-10-23 09:40:15 +0500

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    protecte    @Override
+    d void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      int[] mas = new int[8];
+    }
+}
